5e853498931fefa3b50f57828892fa5e6b1583c9
John Conrad Domingo <johnconrad.domingo@skidata.com>
2018-01-05 16:04:12 +1100

    Initial Checkin

---
+++ SignalR.MessageNotification/Scripts/app/notification.js
@@ -0,0 +1,61 @@
+﻿var app = angular.module("notificationApp",[]);
+var notificationController = app.controller("notificationController", function($scope) {
+
+    //
+    // Initialise
+    //
+
+    // The hubUrl has been hardcoded.
+    // You'd probably want to call $http to get the configurable URL value
+     
+    $.connection.hub.url = "http://localhost:8080/signalr";
+    $scope.notifHub = $.connection.notificationHub; 
+    $.connection.hub.start(); 
+
+    //
+    // Handler Methods
+    //
+
+    $scope.notifHub.client.addNotification = function (message) {
+        
+        var parsedMessage = JSON.parse(message);
+        $scope.notifications.push(parsedMessage);
+        $scope.$apply();
+    };
+
+    $scope.notifHub.client.removeNotification = function (id) {
+        // Get the index based on the Id
+        var thisId = 0;
+        angular.forEach($scope.notifications,
+            function (notification) {
+                if (thisId === notification.id) {
+                    thisId = notification.id;
+                }
+            });
+
+        $scope.notifications.splice(thisId, 1);  
+        $scope.$apply();
+    };
+
+    //
+    // Send Method
+    //
+
+    $scope.readMessage = function (id) {
+
+        // Call the Server to tell it that a message has been read.
+        // The Server will then call removeNotification.
+        $scope.notifHub.server.removeNotification(id);
+    };
+
+    $.connection.hub.disconnected(function () {
+        setTimeout(function () { $.connection.hub.start(); }, 5000);
+    });
+
+    // Startup.
+    // Normally you would want to get all existing messages
+    // We won't do that for this demo
+
+    // For now, let's declare $scope.notifications as an empty array
+    $scope.notifications = [];
+});
---
+++ SignalR.MessageSender/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+ 
+
+namespace SignalR.MessageSender
+{
+    class Program
+    {
+        static HubConnection _hubConnection;
+        static IHubProxy _hubProxy;
+        private static List<NotificationObject> _notificationObjects;
+ 
+        static void Main(string[] args)
+        {
+            // Initialise
+            _notificationObjects = new List<NotificationObject>();
+
+            string url = "http://localhost:8080";
+
+            // First, start SignalR
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine("Server running on {0}", url);
+
+                // Now that SignalR is connected, we want to create a Client Proxy
+                // This enables this same machine to acts as a client enabling it to send data to everyone 
+                // If everything in your application lives in the Web, there's no need for this
+
+                _hubConnection = new HubConnection(url);
+
+                // We reference to the hub
+                _hubProxy = _hubConnection.CreateHubProxy("notificationHub");
+               
+                _hubConnection.Start().Wait();
+
+                var command = Console.ReadLine();
+                while (command.ToLower() != "exit")
+                {
+                    AddNotification(command);
+                    command = Console.ReadLine();
+                } 
+
+
+            }
+        }
+        
+        static void AddNotification(string message)
+        {
+            var newNotification = new NotificationObject {Id = _notificationObjects.Count, Message = message};
+            _notificationObjects.Add(newNotification); 
+
+            _hubProxy.Invoke("AddNotification", newNotification).Wait();
+        }
+
+    }
+
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+
+    [HubName("notificationHub")]
+    public class NotificationHub : Hub
+    { 
+        public void AddNotification(NotificationObject notification)
+        {
+            Clients.All.addNotification(JsonConvert.SerializeObject(notification,
+                new JsonSerializerSettings {ContractResolver = new CamelCasePropertyNamesContractResolver()}));
+        }
+
+        public void RemoveNotification(int id)
+        {
+            // Do you want the Server to do something about this message that you wanted to remove?
+            // You have the ID now. You can now do whatever you want.
+
+            Clients.All.removeNotification(id);
+        }
+    }
+
+    public class NotificationObject
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+
+    }
+      
+}
